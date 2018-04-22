01a173efc4078cb7ea1c8e9efc70e2f0267d1cc2
Martin <martin.me15@yahoo.com>
2018-04-22 21:22:09 +0700

    fixed package design

--- azora/app/controllers/packages_controller.rb
+++ azora/app/controllers/packages_controller.rb
@@ -1,17 +1,18 @@
 class PackagesController < ApplicationController
-  def show
+
+  def index
     @services = Service.all
     @types = ["domestic", "international"]
-    @package = Package.find_by_permalink(params[:permalink])
-    if @package.nil?
-      flash[:error] = "Cannot Find Package with name '#{params[:permalink].humanize}'"
-      if Package.default.blank?
-        redirect_to services_path("incentive-tour")
-      else
-        redirect_to visa_detail_path(Package.default.permalink)
-      end
-      return
+    @packages = Package.where("package_type = '#{params[:type].downcase}'").order("title DESC").
+                        page(params[:page]).per(12)
+    @packages_arr = @packages.each_slice(3).to_a
+  end
+
+  def show
+    respond_to do |format|
+      @package = Package.find_by_permalink(params[:permalink])
+
+      format.js
     end
-    @type = @package.package_type
   end
 end
