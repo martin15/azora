2b0fac0f9d6b7ffaec8b0f705407776e6570c746
Martin <martin@kiranatama.com>
2020-08-24 19:01:08 +0700

    fixed recaptcha

--- azora/config/initializers/recaptcha.rb
+++ azora/config/initializers/recaptcha.rb
@@ -1,4 +1,6 @@
 Recaptcha.configure do |config|
-  config.public_key  = '6LfXCC4UAAAAAOklYVYX6CjOnQw8w5uQSOV5_jH4'
-  config.private_key = '6LfXCC4UAAAAAOZWZiB-PYeKEpN_Y9lR69CByCkx'
+  # config.public_key  = '6LfXCC4UAAAAAOklYVYX6CjOnQw8w5uQSOV5_jH4'
+  # config.private_key = '6LfXCC4UAAAAAOZWZiB-PYeKEpN_Y9lR69CByCkx'
+  config.public_key  = '6LcOocIZAAAAACZEZms2fssrz-Ghvnkui-B16_9I'
+  config.private_key = '6LcOocIZAAAAANBtxkamGb2aqF45CuxQxxef3rYw'
 end
